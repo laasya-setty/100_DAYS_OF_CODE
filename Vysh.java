2e11c2cc17d13d08d1fdfedb9666609f69c43507
laasya-setty <45589527+laasya-setty@users.noreply.github.com>
2020-07-17 19:12:38 +0530

    java
    java

---
+++ Vysh.java
@@ -0,0 +1,19 @@
+ 
+import java.util.*;
+
+public class Vysh{
+ 
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ String a=sc.nextLine();
+ String b=sc.nextLine();
+ String c=sc.nextLine();
+ sc.close();
+ char[] a1=a.toCharArray();
+ char[] b1=b.toCharArray();
+ char[] c1=c.toCharArray();
+ System.out.println(a1);
+ System.out.println(b1);
+ System.out.println(c1);
+ }   
+}
