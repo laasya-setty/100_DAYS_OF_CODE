bce6fd4e98551ca4ac68902e094d1f84bad80e05
Sai Laasya Vabilisetty <likithalaasya@gmail.com>
2020-07-27 13:45:41 +0530

    made changes

--- PrimeNum.java
+++ PrimeNum.java
@@ -1,29 +1,31 @@
 /*Program to check whther a number is prime or not */
-import java.util.Scanner;
+
 
 
 
 public class PrimeNum {
+
+  static  boolean check(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+            
+        }
+        return true;
+    }    
     public static void main(String[] args) {  
-        Scanner s = new Scanner(System.in);  
-        System.out.print("Enter a number : ");  
-        int n = s.nextInt();  
-        if (isPrime(n)) {  
-            System.out.println(n + " is a prime number");  
-        } else {  
-            System.out.println(n + " is not a prime number");  
-        }  
-    }  
-   
-    public static boolean isPrime(int n) {  
-        if (n <= 1) {  
-            return false;  
-        }  
-        for (int i = 2; i < Math.sqrt(n); i++) {  
-            if (n % i == 0) {  
-                return false;  
-            }  
-        }  
-        return true;  
-    }  
- }  
+    if(check(12)){
+        System.out.println("is a prime");
+
+    }
+    else{
+        System.out.println("is not a prime");
+    }
+    
+    }
+
+}
